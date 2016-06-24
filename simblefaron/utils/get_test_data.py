d5d9b05c0cb9acf0e2a7f9a34a5a5ee58fdfee4f
Joan Massich <sik@visor.udg.edu>
2016-06-24 16:06:09 +0200

    Added a dummy image from internet

--- simblefaron/utils/get_test_data.py
+++ simblefaron/utils/get_test_data.py
@@ -2,9 +2,14 @@
 
 from __future__ import print_function
 from __future__ import division
+import urllib
 
 def Get_test_data():
     """A helper function to download the testing data.
+
+    .. todo::
+        implement testing
     """
-    print 'Get_test_data'
+    testfile = urllib.URLopener()
+    testfile.retrieve("http://img.medscapestatic.com/pi/meds/ckb/97/30797tn.jpg", "file.jpg")
 
